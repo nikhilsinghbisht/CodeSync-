7a499e810ab64bbd7fbfac85ced08c18e39cdd55
nikhil <nb9690@gmail.com>
2025-07-30 20:08:18 +0530

    correction done

--- backend/index.js
+++ backend/index.js
@@ -184,11 +184,15 @@ io.on("connection", (socket) => {
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(port, () => {
   console.log("Server is working on port 5000");
 });

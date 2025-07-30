8653cef3352205e4a7f027c03ca364daf5addf91
nikhil <nb9690@gmail.com>
2025-07-30 19:53:23 +0530

    deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'public', // Changed output directory from "dist" to "public"
-  },
+    outDir: 'dist', // <-- Correct output directory for Vercel
+  }
 })
